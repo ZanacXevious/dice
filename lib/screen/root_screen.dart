ff4cb2d7749fd65ce3b0549beea13f825693e360
ZanacXevious <ckongmario21@outlook.com>
2023-03-30 09:17:39 -0600

    dice_004

--- lib/screen/root_screen.dart
+++ lib/screen/root_screen.dart
@@ -29,7 +29,22 @@ class _RootScreenState extends State<RootScreen> with TickerProviderStateMixin {
 }
 
 List<Widget> renderChildren() {
-  return [];
+  return [
+    Container(
+      child: Center(
+          child: Text(
+        'Tab 1',
+        style: TextStyle(color: Colors.white),
+      )),
+    ),
+    Container(
+      child: Center(
+          child: Text(
+        'Tab 2',
+        style: TextStyle(color: Colors.white),
+      )),
+    )
+  ];
 }
 
 BottomNavigationBar renderBottomNavigation() {
